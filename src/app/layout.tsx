a7bf86af183b37f62777b965a1e95894c6814287
Eden Marco <emarco177@gmail.com>
2024-09-18 20:17:51 +0300

    added landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import NextAuthSessionProvider from './components/SessionProvider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dandi Github Analyzer',
+  description: 'Get powerful insights for open source GitHub repositories',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NextAuthSessionProvider>
+          {children}
+        </NextAuthSessionProvider>
+      </body>
+    </html>
+  )
+}
