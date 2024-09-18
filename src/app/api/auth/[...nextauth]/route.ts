8ef7cfbe9ce5f608a5ed08f9ac82529fe7b8c0cc
Eden Marco <emarco177@gmail.com>
2024-09-18 20:34:27 +0300

    annoying bugfix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import { supabase } from "../../../lib/supabaseClient";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
   events: {
